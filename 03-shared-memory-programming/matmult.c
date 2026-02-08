2df12abba2e096e72c6f6ae8faeb5734f672ec9d
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2026-02-08 14:41:12 +0000

    update matmult.c and openmp.c in lecture 03

--- 03-shared-memory-programming/matmult.c
+++ 03-shared-memory-programming/matmult.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-#define N 2000
+#define N 1000
 
 int A[N][N];
 int B[N][N];
 int C[N][N];
 
 int main(int argc, char **argv) {
+    struct timeval start, end, res;
 
     for(int i=0; i<N; i++)
         for(int j=0; j<N; j++) {
@@ -15,12 +17,19 @@ int main(int argc, char **argv) {
             B[i][j] = rand()%100;
         }
 
+    gettimeofday(&start, NULL);
+
     for(int i=0; i<N; i++)
         for(int j=0; j<N; j++) {
             C[i][j] = 0;
             for(int k=0; k<N; k++)
                 C[i][j] = C[i][j] + A[i][k] * B[k][j];
         }
+
+    gettimeofday(&end, NULL);
+    timersub(&end, &start, &res);
+
+    printf("%d x %d matrix multiplication (sequential): %ld.%06ld\n", N, N, res.tv_sec, res.tv_usec);
 
     return 0;
 }
--- 03-shared-memory-programming/openmp.c
+++ 03-shared-memory-programming/openmp.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <omp.h>
 
-#define N 1000
+#define N 2000
 
 int A[N][N];
 int B[N][N];
 int C[N][N];
 
 int main(int argc, char **argv) {
+    struct timeval start, end, res;
 
     for(int i=0; i<N; i++)
         for(int j=0; j<N; j++) {
@@ -16,15 +18,20 @@ int main(int argc, char **argv) {
             B[i][j] = rand()%100;
         }
 
-#pragma omp parallel
-    {
-        for(int i=0; i<N; i++)  // this loop is parallelized automatically
-            for(int j=0; j<N; j++) {
-                C[i][j] = 0;
-                for(int k=0; k<N; k++)
-                    C[i][j] = C[i][j] + A[i][k] * B[k][j];
-            }
+    gettimeofday(&start, NULL);
+
+#pragma omp parallel for
+    for(int i=0; i<N; i++)  // this loop is parallelized automatically
+        for(int j=0; j<N; j++) {
+            C[i][j] = 0;
+            for(int k=0; k<N; k++)
+                C[i][j] = C[i][j] + A[i][k] * B[k][j];
     }
+
+    gettimeofday(&end, NULL);
+    timersub(&end, &start, &res);
+
+    printf("%d x %d matrix multiplication (OMP): %ld.%06ld\n", N, N, res.tv_sec, res.tv_usec);
 
     return 0;
 }
